c27bdfdb9d391dc6aad48bc0dcf1d2a94a919d17
Hackbright Student <user@mini-008.(none)>
2013-03-22 16:08:01 -0700

    first version

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 model.py
 """
 import sqlite3
+import datetime
 
 def connect_db():
     return sqlite3.connect("tipsy.db")
@@ -9,8 +10,9 @@ def connect_db():
 def new_user(db, email, password, name):          
     c = db.cursor()                                     
     query = """INSERT INTO Users VALUES (NULL, ?, ?, ?)"""                                                           
-    c.execute(query, (email, password, name))           
+    result = c.execute(query, (email, password, name))          
     db.commit()
+    return result.lastrowid
 
 def authenticate(db, email, password):
     c = db.cursor()
@@ -25,19 +27,63 @@ def authenticate(db, email, password):
 
 def get_user(db, user_id):
     """Gets a user dictionary out of the database given an id"""
-    pass
+    c = db.cursor()
+    query = """SELECT * from Users WHERE id = ?"""
+    c.execute(query, (user_id))
+    result = c.fetchone()
+    if result:
+        fields = ["id", "email", "password", "username"]
+        return dict(zip(fields,result))
 
 def new_task(db, title, user_id):
     """Given a title and a user_id, create a new task belonging to that user. Return the id of the created task"""
-    pass
+    c = db.cursor()
+    time_created = datetime.datetime.today()
+    query = """INSERT INTO Tasks VALUES (NULL, ?, ?, NULL, ?)"""
+    result = c.execute(query,(title, time_created, user_id))
+    db.commit()
+    return result.lastrowid
+
 
 def complete_task(db, task_id):
     """Mark the task with the given task_id as being complete."""
-    pass
+    c = db.cursor()
+    time_completed = datetime.datetime.now()
+    query = """UPDATE Tasks SET completed_at=? WHERE id = ?"""
+    result = c.execute(query,(time_completed, task_id))
+    db.commit()
+
 
 def get_tasks(db, user_id=None):
     """Get all the tasks matching the user_id, getting all the tasks in the system if the user_id is not provided. Returns the results as a list of dictionaries."""
-    pass
+    c = db.cursor()
+    tasks = []
+    if user_id == None:
+        query = """SELECT * FROM Tasks"""
+        c.execute(query,())
+    else:
+        query = """SELECT * FROM Tasks WHERE user_id = ?"""
+        c.execute(query, (user_id))
+    
+    result = c.fetchall()  
+    
+    if result:
+        for row in result:
+            new_dict = {}
+            new_dict ['id'] = row[0]
+            new_dict ['title'] = row[1]
+            new_dict ['created_at'] = row[2]
+            new_dict ['completed_at'] = row[3]
+            new_dict ['user_id'] = row[4]
+            tasks.append(new_dict)
+        return tasks
+    else:
+        print """No tasks exist"""
+        # else:
+    #     result = c.fetchall()
+    #     fields = []
+    #     return dict(zip(fields,result))
+
 
 def get_task(db, task_id):
     """Gets a single task, given its id. Returns a dictionary of the task data."""
